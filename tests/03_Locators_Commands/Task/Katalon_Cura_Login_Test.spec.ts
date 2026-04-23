f014f37ff6234908c8376ae13a12f3dea35c7543
meetijha <meetijha@gmail.com>
2026-04-23 09:17:58 +0530

    Task

--- tests/03_Locators_Commands/Task/Katalon_Cura_Login_Test.spec.ts
+++ tests/03_Locators_Commands/Task/Katalon_Cura_Login_Test.spec.ts
@@ -1,8 +1,8 @@
 // 1. Got to - >https://katalon-demo-cura.herokuapp.com/
 // 2. Click On the make appointment.  
-// 3. Next page, you are going to enter the username and password
+// 3. Next page, Enter the username and password
 // 4. https://katalon-demo-cura.herokuapp.com/#appointment We need to verify that the URL is now changed to this. 
-// 5. And verify The page contains make appointments. Make Appointment
+// 5. And verify The page contains heading Make Appointment
 
 
 import { test, expect } from "@playwright/test";
@@ -15,9 +15,9 @@ test("Verify login functionality of Katalon Cura website", async ({ page }) => {
     // 2. Click On the make appointment. 
 
      let makeAppointmentButton = page.locator("#btn-make-appointment");
-      await makeAppointmentButton.click();
+     await makeAppointmentButton.click();
 
-    // 3. Next page, you are going to enter the username and password
+    // 3. Next page,  Enter the username and password
    
       let username = page.locator('#txt-username');
       let password= page.locator('#txt-password');
@@ -30,7 +30,7 @@ test("Verify login functionality of Katalon Cura website", async ({ page }) => {
       // 4. Verify the changed URL
       await expect (page).toHaveURL("https://katalon-demo-cura.herokuapp.com/#appointment");
       
-      // 5. And verify The page contains make appointments. Make Appointment
+      // 5. And verify The page contains heading Make Appointment
        let heading = page.locator('.col-sm-12.text-center');
        await expect(heading).toContainText("Make Appointment");
 
