a28782b9fe6d17f96e759548161458ba1d328e8b
meetijha <meetijha@gmail.com>
2026-05-14 08:49:22 +0530

    updated advance svg

--- tests/12_Handle_SVG/250_Advance_SVG_Project.spec.ts
+++ tests/12_Handle_SVG/250_Advance_SVG_Project.spec.ts
@@ -7,16 +7,26 @@ test('Advance SVG Project',async({page})=>{
 
     await page.goto('https://simplemaps.com/svg/country/in');
 
-    let allStates = await page.locator('path').all();
+    // Print all classnames of the states and click on the UP.
+    let allStates = await page.locator('path').all();   
     for (const state of allStates) {
         const className = await state.getAttribute('class');
         if(className){ // checks if className is not null
             console.log(className);
-
+        
          if(className.endsWith('UP')){ // checks if stateEnd ends with 'UP'
-         await state.click();    
-     }
+         await state.click();   
+         console.log("Clicked on UP");       
+     }  
     }
 } 
+
+ // Prints all the names of states
+     let allStateNames = await page.locator('text tspan').all();
+        for(const stateName of allStateNames){
+            const name = await stateName.textContent();
+            console.log(name);
+        }
+        
     await page.waitForTimeout(5000);
 });
